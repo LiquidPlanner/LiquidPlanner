84fab3ff60dca261410a4557c28f36c0ca862770
Mark Holton <holtonma@gmail.com>
2012-06-20 16:53:52 -0700

    added example verifying that updating custom Project fields worked.

--- examples/update_project_custom_field_value.rb
+++ examples/update_project_custom_field_value.rb
@@ -18,34 +18,33 @@ account     = lp.account
 
 say "-" * 40
 # Get the user's first task
-tasks = workspace.tasks(:all, :filter=>'owner_id = me')
+projects = workspace.projects(:all, :filter=>'owner_id = me')
 custom_fields = workspace.custom_fields(:all)
 
-if tasks.empty?
+if projects.empty?
   say "No tasks are assigned to you."
   exit
 else
-  task = tasks.first
+  project = projects.first
 end
 
-say "Workspace: #{workspace.name} - Set a Custom Field Value on the first Task, Milestone, or Event in your space"
-say "#{task.type}: #{task.name}"
-say "* available Custom Task Fields:"
+say "Workspace: #{workspace.name} - Set a Custom Field Value on a Project"
+say "#{project.type}: #{project.name}"
+say "* available Custom Project Fields:"
 
 # allows you to set all custom fields available on this task
 custom_fields.each do |cf|
   cf_name = cf.name
   case cf.category
-    when "task" # custom_fields can be of category: 'task' or 'project'
+    when "project" # custom_fields can be of category: 'task' or 'project'
       say "#{cf.name}"
       cf_name = cf.name
       cf.values.each do |cfv| # options to choose from
         say "#{cfv.label}"
       end
-      cf_value = ask("Type in the Custom Field Value you wish to set on '#{task.name}'?", String)
-      puts "chose: #{cf_value}"
-      # HTTP PUT "'{'task':{'custom_field_values':{'your_task_field':'your_task_field_value'}}}'"
-      task.custom_field_values = {cf_name => cf_value}
-      task.save
+      cf_value = ask("Type in the Custom Field Value you wish to set on '#{project.name}'?", String)
+      say "chose: #{cf_value}"
+      project.custom_field_values = {cf_name => cf_value}
+      project.save
   end    
-end  
+end
